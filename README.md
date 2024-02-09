22b2da343282ed22526f7d2c76022a38a2a27054
abdallahgamal <abdallahgamal840@gmail.com>
2024-02-09 20:28:09 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AirBnB_clone
+description of the project
+# How to start it
+
+# How to use it
+
+# Examples
---
+++ models/base_model.py
@@ -0,0 +1,3 @@
+#  file is the base class of all our models. It contains common elements:
+# attributes: id, created_at and updated_at
+# methods: save() and to_json()
