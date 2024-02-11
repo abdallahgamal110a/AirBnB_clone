a257271ff635b3365197ef4157771f44342c8cff
abdallahgamal <abdallahgamal840@gmail.com>
2024-02-12 01:07:35 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,45 @@
-#  file is the base class of all our models. It contains common elements:
-# attributes: id, created_at and updated_at
-# methods: save() and to_json()
+#!/usr/bin/python3
+import uuid
+import datetime
+
+
+class BaseModel:
+    """class BaseModel that defines all common attributes/methods for other classes
+
+    Returns:
+        str: id,updated_at,created_at,...etc
+    """
+
+    id = None
+    updated_at = None
+    created_at = None
+
+    def __init__(self, *args, **kwargs):
+        """Creates new instances of BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """funcation return string
+
+        Returns:
+            str: [<class name>] (<self.id>) <self.__dict__>_description_
+        """
+        return "[{}] ({}) {}".format((self.__class__.__name__), self.id, self.__dict__)
+
+    def save(self):
+        """the current datetime when an instance is created and it will be updated every time you change your object"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """create a dictionary representation with “simple object type” of our BaseModel
+
+        Returns:
+            dictionary : copy dictionary
+        """
+        dict_copy = self.__dict__.copy()
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy["__class__"] = self.__class__.__name__
+        return dict_copy
