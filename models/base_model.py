969889a0cf07d7be14ac1cac1a5e00885d791853
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-04-07 23:46:09 +0200

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,61 +1,31 @@
 #!/usr/bin/python3
-"""
-This file defines  the BaseModel class which will
-serve as the base of ou model.
-"""
-import uuid
 import datetime
-import models
+import uuid
 
 
 class BaseModel:
-    """class BaseModel that defines all common attributes/methods for other classes
 
-    Returns:
-        str: id,updated_at,created_at,...etc
-    """
-
-    def __init__(self, *args, **kwargs):
-        """Creates new instances of BaseModel"""
+    def __init__(self):
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.datetime.fromisoformat(value)
-                    setattr(self, key, value)
-            return
-        models.storage.new(self)
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def __str__(self):
-        """function return string
-
-        Returns:
-            str: [<class name>] (<self.id>) <self.__dict__>_description_
-        """
-        return "[{}] ({}) {}".format((self.__class__.__name__), self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """the current datetime when an instance is created and it will be updated every time you change your object"""
-        self.updated_at = datetime.datetime.now()
-        models.storage.save()
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def to_dict(self):
-        """create a dictionary representation with “simple object type” of our BaseModel
 
-        Returns:
-            dictionary : copy dictionary
-        """
-        map_objects = {}
+        my_dict = {}
+
         for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                map_objects[key] = value.isoformat()
+            if key == "updated_at" or key == "created_at":
+                my_dict[key] = datetime.datetime.fromisoformat(value).isoformat()
             else:
-                map_objects[key] = value
-        map_objects["__class__"] = self.__class__.__name__
-        return map_objects
+                my_dict[key] = value
+        my_dict["__class__"] = self.__class__.__name__
+
+        return my_dict
