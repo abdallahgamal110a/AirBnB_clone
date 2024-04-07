42364d5d3959058af23bfea5519a411f007fabaa
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-04-08 00:18:47 +0200

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,59 +1,61 @@
 #!/usr/bin/python3
-"""This module contains the class BaseModel"""
-
-import uuid
+"""
+Custom base class for the entire project
+"""
 from datetime import datetime
+import uuid
 import models
 
 
 class BaseModel:
-    """The BaseModel class that defines all common attributes
-            and methods of other classes
+    """Custom base for all the classes in the AirBnb console project
+
+    Arttributes:
+        id(str): handles unique user identity
+        created_at: assigns current datetime
+        updated_at: updates current datetime
+
+    Methods:
+        __str__: prints the class name, id, and creates dictionary
+        representations of the input values
+        save(self): updates instance arttributes with current datetime
+        to_dict(self): returns the dictionary values of the instance obj
+
     """
 
-    def __init__(self, *args, **kwargs):
-        """The initialization function
-            Args:
-                    created_at: datetime - assign with the current
-                    datetime when an instance is created
-                    updated_at: datetime - assign with the current datetime
-                    when an instance is created and it will be updated
-                    every time you change your object
-                    id: string - assign with an uuid when an instance
-                    is created
+    def __init__(self):
+        """Public instance artributes initialization
+        after creation
+
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attrubute values
+
         """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "updated_at" or key == "created_at":
-                    self.__dict__[key] = datetime.fromisoformat(value)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
-
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """
+        Returns string representation of the class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at with
-            the current datetime"""
-
+        """
+        Updates the public instance attribute:
+        'updated_at' - with the current datetime
+        """
         self.updated_at = datetime.now()
-        # models.storage.new(self)
-        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
-
+        """
+        Method returns a dictionary containing all
+        keys/values of __dict__ instance
+        """
         dict_copied = self.__dict__.copy()
         dict_copied["__class__"] = self.__class__.__name__
         dict_copied["created_at"] = self.created_at.isoformat()
