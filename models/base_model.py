25c8f152911ec4403fdb78ef8b0347ae5c84003e
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-04-08 00:07:13 +0200

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,67 @@
 #!/usr/bin/python3
+"""
+Custom base class for the entire project
+"""
 import datetime
 import uuid
+import models
 
 
 class BaseModel:
+    """Custom base for all the classes in the AirBnb console project
+
+    Arttributes:
+        id(str): handles unique user identity
+        created_at: assigns current datetime
+        updated_at: updates current datetime
+
+    Methods:
+        __str__: prints the class name, id, and creates dictionary
+        representations of the input values
+        save(self): updates instance arttributes with current datetime
+        to_dict(self): returns the dictionary values of the instance obj
+
+    """
 
     def __init__(self):
+        """Public instance artributes initialization
+        after creation
+
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attrubute values
+
+        """
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now().isoformat()
         self.updated_at = datetime.datetime.now().isoformat()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
+        """
+        Returns string representation of the class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates the public instance attribute:
+        'updated_at' - with the current datetime
+        """
         self.updated_at = datetime.datetime.now().isoformat()
 
     def to_dict(self):
-
+        """
+        Method returns a dictionary containing all
+        keys/values of __dict__ instance
+        """
         my_dict = {}
 
         for key, value in self.__dict__.items():
             if key == "updated_at" or key == "created_at":
-                my_dict[key] = datetime.datetime.fromisoformat(value).isoformat()
+                my_dict[key] = datetime.datetime.fromisoformat(
+                    value).isoformat()
             else:
                 my_dict[key] = value
         my_dict["__class__"] = self.__class__.__name__
